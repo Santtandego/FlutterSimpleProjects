538300cb74b748f752c79a6565989fe4472d4616
Santtander1 <Santtander1@protonmail.com>
2020-10-18 22:23:09 -0500

    v_3

--- youtube_ui/lib/Components/c_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-  const Color aColor = Color(0xFF18191E);
-  const Color bColor = Color(0xFF5D648A);
-  const Color cColor = Color(0xFF252636);
-  const Color dColor = Color(0xFF808FB1);
-  const Color eColor = Color(0xFFFFFFFF);
