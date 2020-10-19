d07dde8e71fe0149a23050e130eb891174f758a6
Santtander1 <Santtander1@protonmail.com>
2020-10-18 21:12:37 -0500

    v_7

--- modcalcu/lib/Components/c_colors.dart
+++ modcalcu/lib/Components/c_colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const cLeftColor = Color.fromRGBO(246, 4, 44, 1);
 const cRightColor = Color.fromRGBO(20, 4, 246, 1);
-const cPrimaryColor = Color.fromRGBO(212, 21, 242, 1);
+const cPrimaryColor = Color.fromRGBO(39, 183, 0, 1);
--- modcalcu/lib/home_page.dart
+++ modcalcu/lib/home_page.dart
@@ -222,6 +222,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  
 }
