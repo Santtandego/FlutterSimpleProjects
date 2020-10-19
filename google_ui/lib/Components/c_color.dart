6a239fd8cd87b84bcbd74f685e20b32e148e6d1d
Santtander1 <Santtander1@protonmail.com>
2020-10-18 21:44:35 -0500

    v_5

--- google_ui/lib/Components/c_color.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const buttonColor = Color(0xFF4285f4);
-const backgColor  = Color(0xFFFFFFFF);
-const gColor      = Color(0xFF4987EE);
-const oeColor     = Color(0xFFE6483B);
-const ooColor     = Color(0xFFF8BE16);
-const lColor      = Color(0xFF39A757);
--- modcalcu/lib/Components/c_colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const cLeftColor = Color.fromRGBO(246, 4, 44, 1);
-const cRightColor = Color.fromRGBO(20, 4, 246, 1);
-const cPrimaryColor = Color.fromRGBO(212, 21, 242, 1);
